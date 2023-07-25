00b4608806f0dcc89919f375e878418470cc3f89
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-07-25 17:03:24 -0300

    feat:create user

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import {PrismaModule} from '../prisma/prisma.module';
+import { checkEmailExist } from 'src/middlewares/users.middlewares';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports:[PrismaModule]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule{
+  configure(consumer:MiddlewareConsumer){
+    consumer
+    .apply(checkEmailExist)
+    .forRoutes({path:'users', method:RequestMethod.POST},
+    {path:'users', method:RequestMethod.PATCH})
+  }
+}
