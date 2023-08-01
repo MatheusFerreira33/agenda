1842eaa8d97c91bddad368e0bf73cc495f836389
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-08-01 12:22:27 -0300

    fix:error P1001 adjusted

--- back/prisma/migrations/20230726131602_add_on_delete_cascade_table_contacts/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `contacts` DROP FOREIGN KEY `contacts_userId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `contacts` ADD CONSTRAINT `contacts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
